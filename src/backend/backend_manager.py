f53016a74056e2e78451faa683ab3234cb249ea5
GassiGiuseppe <g.gassi@studenti.poliba.it>
2025-11-23 20:31:24 +0100

    backend-endpoint upadated with a better log system

--- src/backend/backend_manager.py
+++ src/backend/backend_manager.py
@@ -1,6 +1,7 @@
-from backend.backend_endpoint import PhotoAPIClient
+from backend.new_backend_endpoint import PhotoAPIClient
 from core.photo_edit_manager import Tailor
 import PIL
+from pathlib import Path
 
 class BackendMananger():
 
@@ -14,4 +15,5 @@ class BackendMananger():
         # up to now is more simple to re-edit here, the corner chooser is into another dev branch
         # when it will be ready, this class could simply receive the already edited photo
         photo = self._editor.prepare_single_photo(photo_path,corner_path)
-        self._backend.upload_pil_background(photo)
+        photo_name = Path(photo_path).name
+        self._backend.upload_pil_background(photo, photo_name)
