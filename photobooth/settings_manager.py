d54748bbdb0736f213bea6bf198dad013dd0e5f4
Vito Di Bari <57833823+vitodibari@users.noreply.github.com>
2026-06-11 10:00:03 +0200

    feat: frame selection from settings

--- photobooth/settings_manager.py
+++ photobooth/settings_manager.py
@@ -135,3 +135,8 @@ class Settings:
         with open(self._settings_path, 'r') as yaml_file:
             yaml_dict = yaml.safe_load(yaml_file)
         return yaml_dict.get('camera_hotfolder_path', '')
+
+    def get_frame_name(self) -> str:
+        with open(self._settings_path, 'r') as yaml_file:
+            yaml_dict = yaml.safe_load(yaml_file)
+        return yaml_dict.get('frame_name', '')
